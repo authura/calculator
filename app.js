84c88b1ad9b459a761cfd71e899a343a1bb7f629
authura <rhoferdevs@gmail.com>
2022-06-13 12:30:11 -0400

    Operator function to calculate arguments

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const displayValue = document.getElementById('display');
-const calcButtons = document.querySelector('.btn');
+const calcButtons = document.querySelector('.button-container');
 const clear = document.getElementById('clear');
 const clearAll = document.getElementById('clear-all');
 const addition = document.getElementById('add');
 const subtraction = document.getElementById('subtract');
 const multiplication = document.getElementById('multiply');
 const division = document.getElementById('divide');
+const digits = document.querySelectorAll('#digit');
+
+calcButtons.addEventListener('click', () => {
+    
+})
 
 
 //Basic Operations
@@ -25,9 +30,34 @@ const multiply = (x, y) => {
 }
 
 const divide = (x, y) => {
-    const quotient = parseFloat(x) / parseFloat(y);;
-    return quotient;
+    if (y === 0) {
+        alert('Cannot divide by zero!')
+    }
+    else {
+        const quotient = parseFloat(x) / parseFloat(y);
+        return quotient;
+    }
+    
 }
+
+const operate = (operator, num1, num2) => {
+    switch (operator) {
+
+        case '+':
+            return add(num1, num2);
+
+        case '-':
+            return subtract(num1, num2);
+
+        case '*':
+            return multiply(num1, num2);
+
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
+
 
 
 
