60fcfb890b8a018288d25f4078142cc73a28b2ea
authura <rhoferdevs@gmail.com>
2022-06-13 11:08:45 -0400

    Basic operations

---
+++ app.js
@@ -0,0 +1,20 @@
+//Basic Operations
+add = (x, y) => {
+    sum = parseFloat(x) + parseFloat(y);
+    return sum;
+}
+
+subtract = (x, y) => {
+    difference = parseFloat(x) - parseFloat(y);
+    return difference;
+}
+
+multiply = (x, y) => {
+    product = parseFloat(x) * parseFloat(y);
+    return product;
+}
+
+divide = (x, y) => {
+    quotient = parseFloat(x) / parseFloat(y);;
+    return quotient;
+}
